f708ea632b092bd548a667f84e80aec5ae030d8a
Hridesh-Srivastava <hrideshsrivastavaa@outlook.com>
2025-08-17 01:14:19 +0530

    feat: update project details for Oddiant TechLabs and Alumni Data System; add new image asset

--- components/sections/projects-section.tsx
+++ components/sections/projects-section.tsx
@@ -14,7 +14,7 @@ const featuredProjects = [
     description:
       "Comprehensive alumni management system built with Next.js and RESTful APIs for efficient data management and networking.",
     image: "/alumni-data.png?height=200&width=300",
-    technologies: ["React", "Tailwind CSS", "Next.js","Node.js", "Express", "MongoDB", "RESTful APIs"],
+    technologies: ["React", "Tailwind CSS", "Next.js","Shadcn ui", "Node.js", "Express", "MongoDB", "RESTful APIs"],
     icon: Users,
     liveUrl: "https://hsst-alumni-frontend.vercel.app/",
     githubUrl: "https://github.com/Hridesh-Srivastava/Alumni-data-frontend",
@@ -23,11 +23,11 @@ const featuredProjects = [
     title: "Oddiant TechLabs",
     description:
       "Intelligent quiz application with automatic submission and performance tracking using MERN stack and machine learning.",
-    image: "/placeholder.svg?height=200&width=300",
-    technologies: ["React", "Node.js", "MongoDB", "Next.js","RESTful APIs","Tailwind CSS"],
+    image: "/oddiant-img.png?height=200&width=300",
+    technologies: ["React", "Node.js", "MongoDB", "Next.js","Shadcn ui", "RESTful APIs","Tailwind CSS"],
     icon: Brain,
-    liveUrl: "#",
-    githubUrl: "#",
+    liveUrl: "https://www.oddiant.com/",
+    githubUrl: "https://github.com/Hridesh-Srivastava/oddiant-techLabs.git",
   },
   {
     title: "Blink Bank Pro",
